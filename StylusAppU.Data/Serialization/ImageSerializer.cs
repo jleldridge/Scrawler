d3dd80973f0933288fc7b09493c226e32739eab4
Luke Eldridge <jleldridge27@gmail.com>
2016-12-20 12:52:57 -0500

    Work on serializing to images.

---
+++ StylusAppU.Data/Serialization/ImageSerializer.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Graphics.Canvas;
+using StylusAppU.Data.Data;
+
+namespace StylusAppU.Data.Serialization
+{
+    public static class ImageSerializer
+    {
+        public static void CreateImage(Page page)
+        {
+            var device = CanvasDevice.GetSharedDevice();
+
+            using (var target = new CanvasRenderTarget(device, (float)page.Width, (float)page.Height, 96.0f))
+            {
+                using (var session = target.CreateDrawingSession())
+                {
+
+                }
+            }
+        }
+    }
+}
