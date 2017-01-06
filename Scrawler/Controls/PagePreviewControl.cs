c3fa264dd91607a446214a02cab277acf99fa7bc
Luke Eldridge <jleldridge27@gmail.com>
2017-01-06 12:56:36 -0500

    Use readonly ink canvas for page grid preview pages.

--- Scrawler/Controls/PagePreviewControl.cs
+++ Scrawler/Controls/PagePreviewControl.cs
@@ -5,6 +5,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.Graphics.Imaging;
 using Windows.UI;
+using Windows.UI.Input.Inking;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
@@ -15,13 +16,14 @@ namespace Scrawler.Controls
     public class PagePreviewControl : Canvas
     {
         private BackgroundControl _background;
-        private Image _ink;
+        private InkCanvas _ink;
 
         public PagePreviewControl()
         {
             Background = new SolidColorBrush(Colors.White);
             _background = new BackgroundControl();
-            _ink = new Image();
+            _ink = new InkCanvas();
+            _ink.InkPresenter.InputProcessingConfiguration.Mode = InkInputProcessingMode.None;
 
             Children.Add(_background);
             Children.Add(_ink);
@@ -29,7 +31,7 @@ namespace Scrawler.Controls
             Loaded += PagePreview_Loaded;
         }
 
-        private async void PagePreview_Loaded(object sender, RoutedEventArgs e)
+        private void PagePreview_Loaded(object sender, RoutedEventArgs e)
         {
             var pageVm = DataContext as PageViewModel;
             if (pageVm != null)
@@ -37,23 +39,15 @@ namespace Scrawler.Controls
                 _background.Width = pageVm.Width;
                 _background.Height = pageVm.Height;
                 _background.BackgroundViewModel = pageVm.BackgroundViewModel;
-                await DrawInk(pageVm);
+                DrawInk(pageVm);
             }
         }
 
-        private async Task DrawInk(PageViewModel pageVm)
+        private void DrawInk(PageViewModel pageVm)
         {
-            var bitmap = InkRenderer.RenderInk(pageVm.StrokeContainer.GetStrokes(), pageVm.Width, pageVm.Height);
-            var outputBitmap = new SoftwareBitmap(
-                BitmapPixelFormat.Bgra8,
-                (int)bitmap.SizeInPixels.Width,
-                (int)bitmap.SizeInPixels.Height,
-                BitmapAlphaMode.Premultiplied);
-            outputBitmap.CopyFromBuffer(bitmap.GetPixelBytes().AsBuffer());
-
-            var source = new SoftwareBitmapSource();
-            await source.SetBitmapAsync(outputBitmap);
-            _ink.Source = source;
+            _ink.Height = pageVm.Height;
+            _ink.Width = pageVm.Width;
+            _ink.InkPresenter.StrokeContainer = pageVm.StrokeContainer;
         }
     }
 }
