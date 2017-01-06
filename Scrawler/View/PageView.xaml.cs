f4b0f0debcc41ded5ec559f2583dcd1b986d3f67
Luke Eldridge <jleldridge27@gmail.com>
2017-01-06 16:41:53 -0500

    Add pen eraser support with custom ink drying.

--- Scrawler/View/PageView.xaml.cs
+++ Scrawler/View/PageView.xaml.cs
@@ -1,15 +1,11 @@
-﻿using System.Threading.Tasks;
-using Windows.UI.Core;
+﻿using Windows.UI.Core;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml;
 using Scrawler.ViewModel;
 using Windows.UI;
 using Windows.UI.Input.Inking;
 using Windows.Foundation;
-using System.ComponentModel;
-using System;
 using Microsoft.Graphics.Canvas.UI.Xaml;
-using Scrawler.Renderers;
 
 namespace Scrawler.View
 {
@@ -23,8 +19,42 @@ namespace Scrawler.View
             InkCanvasElement.InkPresenter.InputDeviceTypes = CoreInputDeviceTypes.Mouse | CoreInputDeviceTypes.Pen;
             _inkSynchronizer = InkCanvasElement.InkPresenter.ActivateCustomDrying();
             InkCanvasElement.InkPresenter.StrokesCollected += InkPresenter_StrokesCollected;
+
+            // this allows eraser strokes to pass through as unprocessed input.
+            InkCanvasElement.InkPresenter.InputProcessingConfiguration.Mode = InkInputProcessingMode.Inking;
+            InkCanvasElement.InkPresenter.UnprocessedInput.PointerPressed += UnprocessedInput_PointerPressed;
+            InkCanvasElement.InkPresenter.UnprocessedInput.PointerMoved += UnprocessedInput_PointerMoved;
+            InkCanvasElement.InkPresenter.UnprocessedInput.PointerReleased += UnprocessedInput_PointerReleased;
+
             DrawingCanvasElement.Draw += DrawingCanvas_OnDraw;
             DataContextChanged += PageView_DataContextChanged;
+        }
+
+        private void UnprocessedInput_PointerPressed(InkUnprocessedInput sender, PointerEventArgs args)
+        {
+        }
+
+        private void UnprocessedInput_PointerMoved(InkUnprocessedInput sender, PointerEventArgs args)
+        {
+            if (args.CurrentPoint.Properties.IsEraser && args.CurrentPoint.IsInContact)
+            {
+                var point = args.CurrentPoint.Position;
+                var eraseRect = new Rect(new Point(point.X - 3, point.Y - 3), new Point(point.X + 3, point.Y + 3));
+                foreach (var stroke in ViewModel.StrokeContainer.GetStrokes())
+                {
+                    if (RectHelper.Intersect(stroke.BoundingRect, eraseRect) != Rect.Empty)
+                    {
+                        stroke.Selected = true;
+                    }
+                }
+
+                ViewModel.StrokeContainer.DeleteSelected();
+                DrawingCanvasElement.Invalidate();
+            }
+        }
+
+        private void UnprocessedInput_PointerReleased(InkUnprocessedInput sender, PointerEventArgs args)
+        {
         }
 
         public PageViewModel ViewModel
@@ -49,11 +79,6 @@ namespace Scrawler.View
         private void PageView_DataContextChanged(FrameworkElement sender, DataContextChangedEventArgs e)
         {
             DrawingCanvasElement.Invalidate();
-            //var pageViewModel = e.NewValue as PageViewModel;
-            //if (pageViewModel != null)
-            //{
-            //    InkCanvasElement.InkPresenter.StrokeContainer = pageViewModel.StrokeContainer;
-            //}
         }
 
         public void ApplyPenOptions(PenOptionsViewModel options)
