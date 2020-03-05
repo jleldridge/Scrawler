9ad4cf8eb5c16c4e1e90e0e12ef0abb4208a2082
luke <jleldridge27@gmail.com>
2020-03-05 13:16:25 -0800

    Skeleton code for rendering a pdf.

---
+++ Scrawler/Renderers/PDFRenderer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+// required Ghostscript.NET namespaces
+using Ghostscript.NET;
+using Ghostscript.NET.Viewer;
+
+namespace Scrawler.Renderers
+{
+    public static class PDFRenderer
+    {
+        public static void RenderPdfToImage(string path, PDFRendererOptions options)
+        {
+        } 
+    }
+
+    public class PDFRendererOptions
+    {
+    }
+}
